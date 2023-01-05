623cd520fc2c9beddbf9b054960cc49e474a75cf
Alejandro Di Stefano <addistefano@live.com.ar>
2023-01-05 19:11:07 -0300

    Implementacion de Swiper

--- js/products.js
+++ js/products.js
@@ -167,14 +167,6 @@ const swiper = new Swiper(".mySwiper", {
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+    }
 });
 //#endregion
