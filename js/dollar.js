dd3d7447bc3d32f3b64356446241e7cb16d1d319
Alejandro Di Stefano <addistefano@live.com.ar>
2023-01-10 22:37:08 -0300

    Consumo de API Dollar Today

--- js/dollar.js
+++ js/dollar.js
@@ -1,26 +1,28 @@
 //#region Consumo de API Dollar Today
 fetch("https://api.bluelytics.com.ar/v2/latest")
 
-.then((respuesta) => respuesta.json())
-.then((data) => {
-    const   divDollar = d.createElement('div'),
-            ulDollar = d.createElement('ul'),            
-            divTiempo = d.querySelectorAll('.tiempo')[0]
+    .then((respuesta) => respuesta.json())
+    .then((data) => {
+        const 
+            divDollar = d.createElement('div'),
+            ulDollar = d.createElement('ul'),
+            divTiempo = d.querySelectorAll('.tiempo')[0],
+            imgDollar = './img/dolar.png'
 
-            divDollar.appendChild(ulDollar)
-            divTiempo.prepend(divDollar)
-            ulDollar.className = 'dollar';
-            divDollar.className = 'divDollar'
+        divDollar.appendChild(ulDollar)
+        divTiempo.prepend(divDollar)
+        ulDollar.className = 'dollar';
+        divDollar.className = 'divDollar'
 
-            dollarValue = data.blue;           
-            
-            ulDollar.innerHTML =            
-                `<li class="titleDollar">Referencia del Dolar hoy:</li>
-                <li>Valor de Compra: <span> $ ${dollarValue.value_avg} .- (ARS)</span></li>            
-                <li>Valor de Venta:  <span>  $ ${dollarValue.value_buy} .- (ARS)</span></li>
-                <li><img src="../img/dolar.png" class="usImg"> </li>
-                `            
-})
+        dollarValue = data.blue;
 
-.catch((error) => ulDollar.innerHTML = `<li>${error}</li>`)
+        ulDollar.innerHTML =
+            `<li class="titleDollar">Referencia del Dolar hoy:</li>
+            <li>Valor de Compra: <span> $ ${dollarValue.value_avg} .- (ARS)</span></li>            
+            <li>Valor de Venta:  <span>  $ ${dollarValue.value_buy} .- (ARS)</span></li>
+            <li><img src="${imgDollar}" class="usImg"> </li>
+            `
+    })
+
+    .catch((error) => ulDollar.innerHTML = `<li>${error}</li>`)
 //#endregion
